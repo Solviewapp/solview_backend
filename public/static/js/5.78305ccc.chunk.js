cd33e41cdc5e5abcb8db80e219b6d4db3047c85e
Solviewapp <93973520+Solviewapp@users.noreply.github.com>
2022-02-02 22:36:35 +0300

    1

--- public/static/js/5.78305ccc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[5],{687:function(a,s,d){}}]);
-//# sourceMappingURL=5.78305ccc.chunk.js.map
