e988dcb15b58fc52f513d1c315d9a4a7f52c2a96
BestSilverTiger <93973520+Solviewapp@users.noreply.github.com>
2022-03-09 06:01:06 +0300

    update

---
+++ public/static/js/5.daf4c670.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[5],{699:function(a,s,d){}}]);
+//# sourceMappingURL=5.daf4c670.chunk.js.map
